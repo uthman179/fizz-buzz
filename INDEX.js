0d595b275198425eaa184dfec66852ee8bec7fec
Giwa <kyzi286@gmail.com>
2023-10-18 17:04:10 +0100

    completed the project

--- INDEX.js
+++ INDEX.js
@@ -1,2 +1,19 @@
+function fizzBuzz() {
+  const result = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
 
+const fizzBuzzArray = fizzBuzz();
+console.log(fizzBuzzArray);
  
